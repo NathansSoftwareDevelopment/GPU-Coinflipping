8f45521ca02195485a37114c5d4d5dee634b5385
Nathan Hogen <nathanhogen2005@gmail.com>
2026-02-21 20:55:26 -0600

    feat: Add user input for target chance of success

--- coinflip.py
+++ coinflip.py
@@ -1,5 +1,12 @@
 def main():
-    pass
+    target_success_chance: float = get_success_chance()
+
+
+
+def get_success_chance() -> float:
+    user_input = float(input("What success chance would you like? "))
+    return user_input
+
 
 if __name__ == "__main__":
     main()
